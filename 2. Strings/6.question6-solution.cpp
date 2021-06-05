726a3f9c81b87b638457435085a76a6e7615cfa5
Piyush Kumar <piyush25032@gmail.com>
2021-06-06 00:03:49 +0530

    format the code using clang

--- 2. Strings/6.question6-solution.cpp
+++ 2. Strings/6.question6-solution.cpp
@@ -1,38 +1,40 @@
-#include<iostream>
-#include<cstring>
+#include <cstring>
+#include <iostream>
 
 using namespace std;
 
-bool isSubSequence(string s1, string s2) {
+bool isSubSequence(string s1, string s2)
+{
 
-   int l1 = s1.length();
-   int l2 = s2.length();
-   int i,j = 0;
-   for(i=0; i<l2;) {
+    int l1 = s1.length();
+    int l2 = s2.length();
+    int i, j = 0;
+    for (i = 0; i < l2;) {
         if (s1[j] != s2[i]) {
             j++;
-        }else {
+        } else {
             j++;
             i++;
         }
 
-        if(j>=l1) {
+        if (j >= l1) {
             break;
         }
-   }
+    }
     return (i == l2);
 }
 
-int main() {
+int main()
+{
     string s1, s2;
 
-    cin>> s1 >> s2;
+    cin >> s1 >> s2;
 
     bool result = isSubSequence(s1, s2);
-    if( result) {
-        cout<<"String 2 is sub sequence of String 1"<<endl;
-    }else {
-        cout<<"String 2 is not sub sequence of String 1"<<endl;
+    if (result) {
+        cout << "String 2 is sub sequence of String 1" << endl;
+    } else {
+        cout << "String 2 is not sub sequence of String 1" << endl;
     }
 
     return 0;
