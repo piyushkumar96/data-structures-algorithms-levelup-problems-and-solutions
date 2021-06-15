39f017f10bae0378aa27eca5f923f0d377cb5e35
Piyush Kumar <piyush25032@gmail.com>
2021-06-15 09:36:31 +0530

    format the code with clang cli tool

--- 4. Sorting and Searching/2.question2-solution.cpp
+++ 4. Sorting and Searching/2.question2-solution.cpp
@@ -8,13 +8,13 @@ vector<int> insertionSort(vector<int> arr)
     int n = arr.size();
     for (int i = 1; i < n; i++) {
         int temp = arr[i];
-        int j=i-1;
-        while(j>=0 && arr[j] > temp) {
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
 
-            arr[j+1] = arr[j];
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1] = temp;
+        arr[j + 1] = temp;
     }
 
     return arr;
