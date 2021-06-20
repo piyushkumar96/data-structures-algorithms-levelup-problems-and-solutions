98b4cda5c5c6a33a9492970985f1e9e699cb2847
Piyush Kumar <piyush25032@gmail.com>
2021-06-20 21:11:30 +0530

    format the code with clang-format cli tool

--- 4. Sorting and Searching/18.question18-solution.cpp
+++ 4. Sorting and Searching/18.question18-solution.cpp
@@ -1,30 +1,30 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include<algorithm>
 
 using namespace std;
 
-bool cmpFunc(const string x, const string y) {
+bool cmpFunc(const string x, const string y)
+{
 
-    return (x+y <= y+x);
+    return (x + y <= y + x);
 }
 
 void smallestString(vector<string>& arr)
 {
     sort(arr.begin(), arr.end(), cmpFunc);
-
 }
 
 int main()
 {
 
-    vector<string> arr { "abb", "a", "aaa", "bsbalj", "c"};
+    vector<string> arr { "abb", "a", "aaa", "bsbalj", "c" };
 
     smallestString(arr);
     cout << "The smallest string is ";
-    for(auto ele : arr) {
-        cout<<ele;
+    for (auto ele : arr) {
+        cout << ele;
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
