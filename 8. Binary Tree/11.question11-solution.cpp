14f819c78f87e9ed485e17215ec7ec1999860496
Piyush Kumar <piyush25032@gmail.com>
2021-07-04 00:11:06 +0530

    Removed not required code.

--- 8. Binary Tree/11.question11-solution.cpp
+++ 8. Binary Tree/11.question11-solution.cpp
@@ -60,17 +60,6 @@ void levelOrderTraversal(Node* root)
     }
 }
 
-int height(Node* root)
-{
-    if (root == NULL) {
-        return 0;
-    }
-    int lh = height(root->left);
-    int rh = height(root->right);
-
-    return max(lh, rh) + 1;
-}
-
 int replaceSum(Node* root)
 {
     if (root == NULL)
